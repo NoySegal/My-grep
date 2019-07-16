5f23d5a7d0d307cb6803f029788d27dfee31f7e5
NoySegal <segalmaterial@gmail.com>
2019-07-16 22:58:38 +0300

    improve structure

--- match.c
+++ match.c
@@ -6,25 +6,26 @@ void string_lower(char *str) {
 	}
 }
 
-bool is_match(char *content, char *pattern) {
-	return strstr(content, pattern);
-}
-
-bool match_by_criteria(char *content, grep_option options, char *pattern) {
+bool match_by_criteria(char *content, switches options, char *pattern) {
+	bool match = false;
 	char *content_dup = strdup(content);
 	if (options.i == true) {
 		string_lower(content_dup);
 		string_lower(pattern);
 	}
-	return is_match(content_dup, pattern);
-}
-
-bool print_by_criteria(line curr_line, print_param settings, grep_option options) {
+	if (options.x == true) {
+		*strchr(content_dup, '\n') = '\0';
+		if (strcmp(content_dup, pattern) == 0) {
+			match = true;
+		}
+	} else {
+		if (strstr(content_dup, pattern) != NULL) {
+			match = true;
+		}
+	}
+	free(content_dup);
 	if (options.v == true) {
-		curr_line.match = !(curr_line.match);
+		return !match;
 	}
-	if (curr_line.match == true) {
-		return true;
-	}
-	return false;
+	return match;
 }
--- match.h
+++ match.h
@@ -4,7 +4,6 @@
 #include "init.h"
 #include <ctype.h>
 
-bool match_by_criteria(char *content, grep_option options, char *pattern);
-bool print_by_criteria(line curr_line, print_param settings, grep_option options);
+bool match_by_criteria(char *content, switches options, char *pattern);
 
 #endif
